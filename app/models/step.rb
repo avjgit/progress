a992fd9435704accea7581ae58438e93b3c07f88
avjgit <avjmail@gmail.com>
2014-01-02 09:36:41 -0800

    step validation

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,4 +1,5 @@
 class Step < ActiveRecord::Base
   belongs_to :track
   attr_accessible :deadline, :description, :grade, :title, :weight
+  validates_presence_of :deadline, :description, :grade, :title, :weight
 end
