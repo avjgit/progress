9d9152fecb0249a2f595d87568c08bf22492eb64
avjgit <avjmail@gmail.com>
2014-01-04 14:20:54 -0800

    users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
 	  user = User.where(email: params[:session][:email].downcase).first
 	  if user && user.authenticate(params[:session][:password])
 	    # Sign the user in and redirect to the user's show page.
+	     sign_in user
+         redirect_to user
 	  else
 	      flash[:error] = 'Invalid email/password combination' # Not quite right!
 	      render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase   }
+  before_create :create_remember_token
+
   attr_accessible :email, :name, :password, :password_confirmation
   validates_presence_of :email, :name
   validates_length_of :name, :maximum => 50
   validates_uniqueness_of :email, :case_sensitive => false
   has_secure_password
   validates_length_of :password, :minimum => 5
+
+    def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
