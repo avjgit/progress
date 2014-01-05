fc3570b424f1fa84be1d841e34fc4f059919f506
avjgit <avjmail@gmail.com>
2014-01-05 03:57:55 -0800

    current user id

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,10 @@ module SessionsHelper
     @current_user ||= User.where(remember_token: remember_token).first
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def signed_in?
     !current_user.nil?
   end
